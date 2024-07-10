a18fc2dd030477897feb580df894057cef4e70be
atharnaqash <athar1991pak@yahoo.com>
2024-07-10 14:41:43 +0200

    implement one-way data binidng to NgModel

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuItem} from '../inza/data/menu-item.model';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+  // this ! tells angular that the value is not initialized but will get a value in ngOnInit rather than constructor()
+  menuItem!: MenuItem;
 
+  ngOnInit() {
+    this.menuItem =  {
+      id: "ABS12X",
+      name: "Margherita Pizza",
+      price: 8.99,
+      isVegetarian: true,
+      promo: 'limited',
+      image: "margherita_pizza.jpg"
+    }
+  }
+
+  onCreate(menuItem: MenuItem) {
+    console.log('app-admin, onCreate()', menuItem)
+  }
 }
--- src/app/admin/components/inza-form/inza-form.component.ts
+++ src/app/admin/components/inza-form/inza-form.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {MenuItem} from '../../../inza/data/menu-item.model';
 
 @Component({
   selector: 'app-inza-form',
@@ -7,18 +8,21 @@ import {NgForm} from '@angular/forms';
   styleUrl: './inza-form.component.scss'
 })
 export class InzaFormComponent {
+  @Input() menuItem!: MenuItem;
+  @Output() create = new EventEmitter<MenuItem>();
 
-  icons: string[] = [
+  images: string[] = [
     'food-cheezy.jpg',
     'mangoo-juice.jpg',
-    'dhuram.jpg'
+    'dhuram.jpg',
+    'margherita_pizza.jpg'
   ]
-
 
   handleSubmit(form: NgForm) {
     if (form.valid) {
       console.log(form.value)
-      alert(JSON.stringify(form.value))
+      //alert(JSON.stringify(form.value))
+      this.create.emit(form.value);
     }
   }
 }
